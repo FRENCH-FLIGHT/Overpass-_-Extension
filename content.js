92f88130ecb03b2a781235ddbf33f33750cf82ce
FRENCH-FLIGHT <98184876+FRENCH-FLIGHT@users.noreply.github.com>
2026-07-07 14:17:08 +0200

    Update version number in content.js comments

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 /**
- * Overpass v3.5.0 – content.js  (ISOLATED world, run_at: document_start)
+ * Overpass v3.6.0 – content.js  (ISOLATED world, run_at: document_start)
  *
  * Rôle : pont sécurisé entre chrome.storage/runtime et inject.js (MAIN world).
  *
